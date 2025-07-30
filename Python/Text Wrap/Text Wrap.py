198844392cec3fa8a9ff4788e6cc1491fb225af6
Sajir-Dwelling <sifaz20075@gmail.com>
2025-07-30 21:12:09 +0600

    Add files via upload

---
+++ Python/Text Wrap/Text Wrap.py
@@ -0,0 +1,13 @@
+import textwrap
+
+def wrap(string,max_width):
+    for i in range(0, len(string)+1, max_width):
+        chunk = string[i:i+max_width]
+        if len(chunk) == max_width:
+            print(chunk)
+        else:
+            return chunk
+if __name__=="__main__":
+    string, max_width = input(), int(input())
+    wrap_text = wrap(string,max_width)
+    print(wrap_text)
