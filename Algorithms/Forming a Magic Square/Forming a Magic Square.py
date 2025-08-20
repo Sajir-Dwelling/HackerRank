39bf0d70645d6e4b8260c29edd2d1b12f06d1bf3
Sajir-Dwelling <sifaz20075@gmail.com>
2025-08-20 16:00:57 +0600

    Algorithm

---
+++ Algorithms/Forming a Magic Square/Forming a Magic Square.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'formingMagicSquare' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY s as parameter.
+#
+
+def formingMagicSquare(s):
+    # Write your code here
+    magic_squares = [
+                    [[8,1,6],[3,5,7],[4,9,2]],  #Original
+                    [[6,1,8],[7,5,3],[2,9,4]],  #Reflection
+                    [[4,9,2],[3,5,7],[8,1,6]],  #Rotation
+                    [[2,9,4],[7,5,3],[6,1,8]],  #Reflection+rotation
+                    [[8,3,4],[1,5,9],[6,7,2]],  #Another reflection
+                    [[4,3,8],[9,5,1],[2,7,6]],  #Reflection above
+                    [[6,7,2],[1,5,9],[8,3,4]],  #Another rotation
+                    [[2,7,6],[9,5,1],[4,3,8]]   #Reflection above
+    ]
+
+    min_cost = float("inf")
+
+    for magic in magic_squares:
+        cost = 0
+        for i in range(3):
+            for j in range(3):
+                cost += abs(magic[i][j]-s[i][j])
+        if cost < min_cost:
+            min_cost = cost
+    return min_cost
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = []
+
+    for _ in range(3):
+        s.append(list(map(int, input().rstrip().split())))
+
+    result = formingMagicSquare(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ Algorithms/Picking Numbers/Picking Numbers.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    a.sort()
+    max_length = 0
+    i = 0
+
+    for j in range(len(a)):
+        while abs(a[j]-a[i])>1:
+            i+=1
+        max_length = max(max_length,j-i+1)
+    return max_length
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
