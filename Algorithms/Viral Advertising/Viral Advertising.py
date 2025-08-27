61f0c96e8862e2cbed0ec60b87f2cd2860783915
Sajir-Dwelling <sifaz20075@gmail.com>
2025-08-27 15:57:48 +0600

    Algorithm

---
+++ Algorithms/Viral Advertising/Viral Advertising.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    # Write your code here
+    shared = 5
+    list_likes = []
+    likes = 0
+    for i in range(1,n+1):
+        likes = shared//2
+        list_likes.append(likes)
+        shared = likes*3
+    return sum(list_likes)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
